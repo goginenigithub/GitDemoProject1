7d2f5cf86991a117abf3224eb818d2df3d7f33aa
HP <HP@DESKTOP-RH13O9U>
2021-09-06 18:36:58 -0400

    Add two ne files to branchX

---
+++ GitDemoProject1/src/defaultPackage/Test2.java
@@ -0,0 +1,18 @@
+package defaultPackage;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String hungry="Yes";
+		if (hungry=="Yes") {
+			System.out.println("eat some thing");
+		}else {
+			System.out.println("do work1");
+			
+			
+			
+						
+		}
+	}
+
+}
