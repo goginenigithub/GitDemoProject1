2e3b13f6e0445f862f2350508fa5cb373f361651
HP <HP@DESKTOP-RH13O9U>
2021-09-06 19:31:26 -0400

    add new cond

--- GitDemoProject1/src/defaultPackage/Test2.java
+++ GitDemoProject1/src/defaultPackage/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		String hungry="Yes";
 		if (hungry=="Yes" || hungry=="Y") {
+			
 			System.out.println("eat some thing");
 		}else {
 			System.out.println("do work1");
